04eecb49a1cc304e97fb5c7f643664a6c4532340
HoangHung <hoanghungg1602@gmail.com>
2021-04-17 11:18:38 +0700

    upe
    update

---
+++ PHP_Store/login.php
@@ -0,0 +1,116 @@
+﻿
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    include_once('./head.php');
+    
+?>
+
+
+<body>
+    <div class="wrapper">
+        <?php
+            include_once('./header.php');
+            require_once('./models/login_class.php');
+            if(isset($_POST['btn_login'])){
+                $username = $_POST['email'];
+                $password = $_POST['pass'];
+               
+                
+                $data = Admin_Login::login($username,$password);
+                
+                if(count($data)==0){
+                    messagebox("Check your login infomation again !");
+                }else{
+                    foreach($data as $item){
+                        
+                        if($item['Level'] != 0){
+                            //echo $item['Level'];
+                            echo  "<script>
+                                    var id = ".$item['ID'].";
+                                    var c_name ='User='
+                                    var d = new Date();
+                                    d.setTime(d.getTime() + (1*24*60*60*1000));
+                                    var expiresdate = 'expires='+ d.toUTCString();
+                                    var txt = 'User='+id+';expires='+expiresdate;
+                                    document.cookie = c_name + id+';'+expiresdate
+                                </script>";
+                                // foreach ($_COOKIE as $key=>$val)
+                                // {
+                                //   echo $key.' is '.$val."<br>\n";
+                                // }
+                            
+                           
+                            echo "<script type='text/javascript'> document.location = './SB_Admin/dashboard.php?id=".$item['ID']."'; </script>";
+                        }else{
+                            echo  "<script>
+                                    var id = ".$item['ID'].";
+                                    var c_name ='User='
+                                    var d = new Date();
+                                    d.setTime(d.getTime() + (1*24*60*60*1000));
+                                    var expiresdate = 'expires='+ d.toUTCString();
+                                    var txt = 'User='+id+';expires='+expiresdate;
+                                    document.cookie = c_name + id+';'+expiresdate
+                                </script>";
+                            echo "<script type='text/javascript'> document.location = './checkout.php?id=".$item['ID']."'; </script>";
+                        }
+                        
+                    }
+                }
+                
+                
+            }
+        ?>
+        <!-- Categorie Menu & Slider Area End Here -->
+        <!-- Breadcrumb Start -->
+        <div class="breadcrumbs">
+		<div class="container">
+			<ol class="breadcrumb breadcrumb1 animated wow slideInLeft animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: slideInLeft;">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+				<li class="active">Account</li>
+			</ol>
+		</div>
+	</div>
+<div class="account">
+	<div class="container">
+		<h2>Account</h2>
+		<div class="account_grid">
+			   <div class="col-md-6 login-right">
+				<form action="#" method="post">
+
+					<span>Email Address</span>
+					<input type="text" name="email"> 
+				
+					<span>Password</span>
+					<input type="password" name="password"> 
+					<div class="word-in">
+				  		<a class="forgot" href="#">Forgot Your Password?</a>
+				 		 <input type="submit" value="Login">
+				  	</div>
+			    </form>
+			   </div>	
+			    <div class="col-md-6 login-left">
+			  	 <h4>NEW CUSTOMERS</h4>
+				 <p>By creating an account with our store, you will be able to move through the checkout process faster, store multiple shipping addresses, view and track your orders in your account and more.</p>
+				 <a class="acount-btn" href="register.php">Create an Account</a>
+			   </div>
+			   <div class="clearfix"> </div>
+			 </div>
+	</div>
+</div>
+                    <!-- Returning Customer End -->
+                </div>
+                <!-- Row End -->
+            </div>
+            <!-- Container End -->
+        </div>
+        <!-- LogIn Page End -->
+        
+       
+    </div>
+    <?php
+            include_once("./footer.php");
+    ?>
+</body>
+
+</html>
---
+++ PHP_Store/register.php
@@ -0,0 +1,114 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<?php
+include_once('./head.php');
+require_once(__DIR__ . "/models/login_class.php")
+?>
+
+<body>
+
+    <div class="wrapper">
+        <?php
+        include_once('./header.php');
+        if (isset($_POST['btn_submit'])) {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $address = $_POST['address'];
+            // $phone = $_POST['phone'];
+            $pass_1 = $_POST['password'];
+            $pass_2 = $_POST['password_1'];
+            if (strlen($name) < 1) {
+                messagebox('Please enter your name.');
+            } elseif (strlen($email) < 1) {
+                messagebox('Please enter your email.');
+            } elseif (strlen($address) < 1) {
+                messagebox('Please enter your address.');
+            // } elseif (strlen($phone) < 1) {
+                // messagebox('Please enter your phonenumber.');
+            } elseif (strlen($pass_1) < 1 || strlen($pass_2) < 1) {
+                messagebox('Please enter your password');
+            } elseif (strlen($pass_1) < 8) {
+                messagebox('Password too short !\nPassword must between 8 and 30.');
+            } elseif (strlen($pass_1) > 30) {
+                messagebox('Password too long !\nPassword must between 8 and 30.');
+            } elseif (strcmp($pass_1, $pass_2) != 0) {
+                messagebox('Password not match !');
+            } else {
+                $register_cus = new Admin_Login($name, $email,$email, $address, $pass_1);
+                $result = $register_cus->insert_customer();
+                if (!$result) {
+                    header('Location: register.php?failure');
+                    messagebox('Register failed.');
+                } else {
+                    header('Location: register.php?inserted');
+                    messagebox('Register success.');
+                }
+            }
+        }
+        ?>
+        <div class="breadcrumbs">
+            <div class="container">
+                <ol class="breadcrumb breadcrumb1 animated wow slideInLeft animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: slideInLeft;">
+                    <li><a href="./index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+                    <li class="active">Register</li>
+                </ol>
+            </div>
+        </div>
+        <!-- Container End -->
+    </div>
+    <!-- Breadcrumb End -->
+    <!-- Register Account Start -->
+    <div class="container">
+        <div class="register">
+            <h2>Register</h2>
+            <form  method="post">
+                <div class="col-md-6  register-top-grid">
+
+                    <div class="mation">
+                        <span>Your Name</span>
+                        <input type="text" id="name" name="name">
+
+                        <span>Email Address</span>
+                        <input type="text" id="email" name="email">
+
+                        <span>Address</span>
+                        <input type="text" id="address" name="address">
+
+                     
+                    </div>
+                    <!-- <div class="clearfix"> </div>
+                    <a class="news-letter" href="#">
+                        <label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i> </i>Sign Up</label>
+                    </a> -->
+                </div>
+                <div class=" col-md-6 register-bottom-grid">
+                    <div class="mation">
+                        <span>Password</span>
+                        <input type="password" id="password" name="password">
+                        <span>Confirm Password</span>
+                        <input type="password" id="password_1" name="password_1">
+                    </div>
+                </div>
+                <div class="clearfix"> </div>
+                
+                    <input type="submit" name="btn_submit" value="Register">
+                    <div class="clearfix">
+            </form>
+
+            <!-- <div class="register-but">
+                <form action="#" method="post">
+                    <input type="submit" name="btn_submit" value="Register">
+                    <div class="clearfix"> </div>
+                </form>
+            </div> -->
+        </div>
+    </div>
+    <!-- Container End -->
+    </div>
+    <?php
+    include_once('./footer.php');
+    ?>
+
+</body>
+
+</html>
